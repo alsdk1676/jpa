d8ff6d7184df5c3bfb89fd3c9604e23ec9f484c7
alsdk1676 <alsdk6761@gmail.com>
2025-06-10 17:48:51 +0900

    jpa 0610

--- src/main/java/com/app/basic/domain/entity/Reply.java
+++ src/main/java/com/app/basic/domain/entity/Reply.java
@@ -8,13 +8,14 @@ import lombok.*;
 // 1. PK키가 필요하다
 // 2. 기본 생성자, 초기화 생성자, Getter, Setter, ToString 재정의, HashEquals
 @NoArgsConstructor @AllArgsConstructor
-@Getter @Setter @ToString @EqualsAndHashCode
+@Getter @Setter @ToString @EqualsAndHashCode(onlyExplicitlyIncluded = true) // 지정한 컬럼만 비교
 @Entity @Table(name = "TBL_REPLY")
 public class Reply {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
+//    @EqualsAndHashCode.Include 포함
     private Long id;
+//    @EqualsAndHashCode.Exclude 제거
     private String replyContent;
     private String replyWriter;
     @Enumerated(EnumType.STRING)
--- src/test/java/com/app/basic/domain/entity/ReplyTest.java
+++ src/test/java/com/app/basic/domain/entity/ReplyTest.java
@@ -21,10 +21,11 @@ class ReplyTest {
     @Test
     public void replyInsertTest() {
         Reply reply = new Reply();
-        reply.setReplyContent("내용4");
-        reply.setReplyWriter("유저4");
+        reply.setReplyContent("게시글3");
+        reply.setReplyWriter("서민아");
         reply.setReplyStatus(ReplyStatus.PUBLIC);
 
+//        영속
         entityManager.persist(reply);
         log.info(reply.toString());
     }
@@ -49,6 +50,22 @@ class ReplyTest {
         Reply foundReply = entityManager.find(Reply.class, 52L);
         entityManager.remove(foundReply);
         log.info(foundReply.toString());
+    }
+
+//    JPA QL
+    @Test
+    public void replySelectAllTest() {
+//        Reply foundReply = entityManager.find(Reply.class, 1L); // find : 단일 조회
+//        JPA QL
+//        1) *(와일드 카드) 사용이 불가능하다.
+//        테이블에 알리아스(소문자)를 붙여서 모든 데이터를 조회할 수 있다.
+//        2) FROM절에서 가져오는 테이블은 DB의 쿼리 테이블이 아니라 엔터티 객체이다.
+//        3) 타입을 알려주어야 한다.
+        entityManager.createQuery("SELECT r FROM Reply r").getResultList();
+        log.info("JPA QL 데이터 조회 : {}", entityManager.createQuery("SELECT r FROM Reply r", Reply.class).getResultList().toString());
+
+        entityManager.createQuery("SELECT r FROM Reply r WHERE r.id = :id", Reply.class).setParameter("id", 152L).getSingleResult().toString();
+        log.info("단일 행 조회 : {}", entityManager.createQuery("SELECT r FROM Reply r WHERE r.id = :id", Reply.class).setParameter("id", 152L).getSingleResult().toString());
     }
 
 }
